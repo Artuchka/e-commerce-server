87358aac1830d3c3de31c9adab724e3b8b8a3f71
JohnDoeee <yandex949@gmail.com>
2022-12-15 14:44:43 +0300

    roles for product

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -7,16 +7,20 @@ const {
 	deleteProduct,
 	uploadImage,
 } = require("../controllers/productController")
+const { roleMiddleware } = require("../middleware/authMiddleware")
 const router = express.Router()
 
-router.route("/").get(getAllProducts).post(createProduct)
+router
+	.route("/")
+	.get(getAllProducts)
+	.post(roleMiddleware("admin"), createProduct)
 
-router.route("/uploadImage").post(uploadImage)
+router.route("/uploadImage").post(roleMiddleware("admin"), uploadImage)
 
 router
 	.route("/:id")
 	.get(getSingleProduct)
-	.patch(updateProduct)
-	.delete(deleteProduct)
+	.patch(roleMiddleware("admin"), updateProduct)
+	.delete(roleMiddleware("admin"), deleteProduct)
 
 module.exports = { productRouter: router }
